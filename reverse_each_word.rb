9048a80e2a9ccf53b1a7afa6156171ad5bd4d83a
Nina Angelo <ninacelie@gmail.com>
2019-12-11 18:26:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string) #method takes in a string as an arguement
   array = string.split(" ") #split converts our string into an array so we can use an enumerator
   array.collect do |stg| #collect changes our return value
-    word.reverse! #reverse
+    word.reverse! #reverse without changing order
   end
   array.join(" ")
 end
