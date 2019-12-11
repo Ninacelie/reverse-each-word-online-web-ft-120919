e67379b2bae1b5bdd76d80c5f88ed81bf3a77b3e
Nina Angelo <ninacelie@gmail.com>
2019-12-11 18:25:51 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-def reverse_each_word(string)
-  array = string.split("")
-  array.collect do |stg|
-    word.reverse!
+def reverse_each_word(string) #method takes in a string as an arguement
+  array = string.split(" ") #split converts our string into an array so we can use an enumerator
+  array.collect do |stg| #collect changes our return value
+    word.reverse! #reverse 
   end
-  array.join("")
+  array.join(" ")
 end
