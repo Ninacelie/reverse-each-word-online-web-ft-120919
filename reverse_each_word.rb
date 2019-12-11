c39ab849e65d4e253319e7e37f4397e2e1daca2d
Nina Angelo <ninacelie@gmail.com>
2019-12-11 18:41:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 first_array = string.split (" ")
 return_array = []
 first_array.each do |string|
-  return_array << string.reverse 
+  return_array << string.reverse
 end
 return_array.join (" ")
 end
